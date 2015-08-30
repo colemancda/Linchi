53876cb36a68bd366952c1635e7ec44601cab080
Kaan Dedeoglu <kaandedeoglu@me.com>
2015-08-30 18:47:49 +0300

    Use the default String values, no need to retype

--- Linchi/HTTP-Definitions/HTTPMethod.swift
+++ Linchi/HTTP-Definitions/HTTPMethod.swift
@@ -5,13 +5,13 @@
 
 /// Enum describing an HTTPMethod
 public enum HTTPMethod: String {    
-    case GET = "GET"
-    case POST = "POST"
-    case HEAD = "HEAD"
-    case DELETE = "DELETE"
-    case OPTIONS = "OPTIONS"
-    case PUT = "PUT"
-    case CONNECT = "CONNECT"
-    case PATCH = "PATCH"
-    case TRACE = "TRACE"
+    case GET
+    case POST
+    case HEAD
+    case DELETE
+    case OPTIONS
+    case PUT
+    case CONNECT
+    case PATCH
+    case TRACE
 }
